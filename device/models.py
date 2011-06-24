3525b33f90e1c820dfe9144672ea4615e286f8c5
John Matthew <john.matthew@nbcuni.com>
2011-06-24 10:14:27 -0700

    Changed relation

--- device/models.py
+++ device/models.py
@@ -1,16 +1,18 @@
 from django.db import models
 
 # Create your models here.
+class Level(models.Model):
+    name = models.CharField(max_length=20)
+        
+    def __unicode__(self):
+        return self.level
+
 class Host(models.Model):
     name      = models.CharField(max_length=100)
     ipaddress = models.IPAddressField()
     
+    level     = models.ForeignKey(Level)
+    
     def __unicode__(self):
         return self.name
     
-class Level(models.Model):
-    host  = models.ForeignKey(Host)
-    level = models.CharField(max_length=20)
-    
-    def __unicode__(self):
-        return self.level

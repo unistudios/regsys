f6b6b9e3d834757564dbb34095c4198fbd320abc
John Matthew <john.matthew@nbcuni.com>
2011-06-24 12:07:09 -0700

    JSON data for application levels.

--- application/admin.py
+++ application/admin.py
@@ -1,6 +1,7 @@
-from application.models import Application, Owner
+from application.models import Application,Owner,Level
 
 from django.contrib import admin
 
 admin.site.register(Application)
 admin.site.register(Owner)
+admin.site.register(Level)

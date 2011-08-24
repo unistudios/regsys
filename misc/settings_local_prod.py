04dcd661d5f2eea48e922d2ee929592ec0af3573
John Matthew <John.matthew@nbcuni.com>
2011-08-24 15:53:16 -0700

    created settings_local*.py copies in misc folder.`

---
+++ misc/settings_local_prod.py
@@ -0,0 +1,12 @@
+PROJECT_PATH = "/opt/regsys"
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'fnf',
+        'USER': 'fnf',
+        'PASSWORD': 'fnf_pw',
+        'HOST': '',
+        'PORT': '',
+    }
+}
